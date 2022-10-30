ab19d37bef29e6cce82bde449dff64e743b775de
Vladimir Kovalenko <dante77758@gmail.com>
2022-10-31 00:48:47 +0200

    add file modal.html  in index.html, add modal.js in index.js, add in index.scss file modal.scss, in mixins components.

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,16 @@
 (() => {
   const refs = {
-    openModalBtnList: document.querySelectorAll('[data-modal-open]'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),
   };
 
-  // refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.openModalBtnList.forEach(element => {
-    element.addEventListener('click', toggleModal);
-  });
-
+  refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
