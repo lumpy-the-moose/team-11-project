0dc14533e261bd321c22e1956a793764cbe4449b
AndriiKachan1996 <realandryxa1996@gmail.com>
2022-11-02 14:26:20 +0200

    slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './js/modal.js';
+
+import Swiper, { Navigation } from 'swiper';
+Swiper.use([Navigation,]);
+
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+  
+  
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
