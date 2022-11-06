1e9dd16bfb0dae3c95ce3ab81912a16c48fded8b
Vladimir Kovalenko <dante77758@gmail.com>
2022-11-06 19:12:24 +0200

    add js no scroll

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,6 +3,7 @@
     openModalBtnList: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),
   };
 
   // refs.openModalBtn.addEventListener("click", toggleModal);
@@ -14,5 +15,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
